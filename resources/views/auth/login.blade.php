a9614f232070974450e8a84ee4b426195228afe7
Temitope Olotin <temitope.olotin@andela.com>
2017-01-14 09:07:30 +0100

    Add softdeleteting trait

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+                <div class="panel-heading text-center">Chemistry Dept. Equipment User System</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
@@ -43,8 +43,8 @@
                                     Login
                                 </button>
 
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">
-                                    Forgot Your Password?
+                                <a class="btn btn-link" href="{{ url('/register') }}">
+                                    Need to signup?
                                 </a>
                             </div>
                         </div>
