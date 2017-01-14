414c00cb3f14e9bdc39bc033628ed0a5d8d43b48
Temitope Olotin <temitope.olotin@andela.com>
2017-01-14 19:21:06 +0100

    Add request preview layout

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -16,59 +16,8 @@
                         </h4>
                     </div>
                     <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
-                        <div class="panel-body">
-                            <form class="form-horizontal">
-                                <div class="form-group">
-                                    <label for="name" class="col-sm-2 control-label">Name</label>
-                                    <div class="col-sm-10">
-                                        <input type="email" class="form-control" id="name" placeholder="Name">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="email" class="col-sm-2 control-label">Email</label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control" id="email" placeholder="Email">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="phone" class="col-sm-2 control-label">Phone</label>
-                                    <div class="col-sm-10">
-                                        <input type="email" class="form-control" id="phone" placeholder="Phone">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="office" class="col-sm-2 control-label">Office</label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control" id="office" placeholder="Office">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label  class="col-sm-4 control-label pull-left">Change password</label>
-                                </div>
-                                <div class="form-group">
-                                    <label for="c_password" class="col-sm-2 control-label">Enter Current Password</label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control" id="c_password" placeholder="Enter Current Password">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="new_password" class="col-sm-2 control-label">Enter New Password</label>
-                                    <div class="col-sm-10">
-                                        <input type="password" class="form-control" id="new_password" placeholder="Enter New Password">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="com_password" class="col-sm-2 control-label">Confirm New Password</label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control" id="com_password" placeholder="Confirm New Password">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="col-sm-offset-2 col-sm-10">
-                                        <button type="submit" class="btn btn-default">Save</button>
-                                    </div>
-                                </div>
-                            </form>
+                    <div class="panel-body">
+                        @include('admin.basic_info.edit_basic_info')
                         </div>
                     </div>
                 </div>
@@ -83,6 +32,7 @@
                     </div>
                     <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
                         <div class="panel-body">
+                        @include('admin.training_requests.training_request')
                         </div>
                     </div>
                 </div>
---
+++ resources/views/admin/training_requests/training_request.blade.php
@@ -0,0 +1,71 @@
+<form class="form-horizontal">
+    <div class="form-group">
+        <label for="name" class="col-sm-2 control-label">Equipment</label>
+        <div class="col-sm-10">
+            <select name="equipment" id="equipment" class="form-control" required="required">
+                <option value="0">Select Equipment</option>
+            </select>
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-sm-12">
+            <table class="table table-responsive">
+                <tbody>
+                    <tr>
+                        <td>Larry</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <td>Otto</td>
+                        <td>
+                            <div class="form-group">
+                                <input type="checkbox" class="form-control" id="select">
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Larry</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <td>Otto</td>
+                        <td>
+                            <div class="form-group">
+                                <input type="checkbox" class="form-control" id="select">
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="name" class="col-sm-2 control-label">Date of Training session</label>
+        <div class="col-sm-3">
+            <select name="equipment" id="equipment" class="form-control" required="required">
+                <option value="0">Select Year</option>
+            </select>
+        </div>
+        <div class="col-sm-3">
+            <select name="equipment" id="equipment" class="form-control" required="required">
+                <option value="0">Select Month</option>
+            </select>
+        </div>
+        <div class="col-sm-3">
+            <select name="equipment" id="equipment" class="form-control" required="required">
+                <option value="0">Select Day</option>
+            </select>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="location" class="col-sm-2 control-label">Location</label>
+        <div class="col-sm-10">
+            <input type="text" class="form-control" id="location" placeholder="Location">
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-large btn-default">Send a confirmation email</button>
+        </div>
+    </div>
+</form>
