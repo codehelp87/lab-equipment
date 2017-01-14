e310aa158b55b53f324abfe264319c0cf4676259
Temitope Olotin <temitope.olotin@andela.com>
2017-01-14 19:45:02 +0100

    Change color to default

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,7 +39,7 @@
                         </div>
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-default">
                                     Login
                                 </button>
 
