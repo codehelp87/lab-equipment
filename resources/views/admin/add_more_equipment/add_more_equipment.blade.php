03dfbcc4df4a9461c0ed11597a8bdbf2b710bc52
Temitope Olotin <temitope.olotin@andela.com>
2017-01-16 00:44:29 +0100

    Format html

--- resources/views/admin/add_more_equipment/add_more_equipment.blade.php
+++ resources/views/admin/add_more_equipment/add_more_equipment.blade.php
@@ -1,38 +1,38 @@
-<form class="form-horizontal">
+<form class="form-horizontal" id="add_more_equipment">
     <div class="form-group">
         <label for="name" class="col-sm-2 control-label">Title of Equipment</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" id="title" placeholder="Name" value="">
+            <input type="text" class="form-control" id="title" name="title" placeholder="Title of Equipment" value="">
         </div>
     </div>
     <div class="form-group">
         <label for="model" class="col-sm-2 control-label">Model No</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" id="model" placeholder="Model No" value="">
+            <input type="text" class="form-control" id="model_no" name="model_no" placeholder="Model No" value="">
         </div>
     </div>
     <div class="form-group">
         <label for="maker" class="col-sm-2 control-label">Maker</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" id="maker" placeholder="Maker" value="">
+            <input type="text" class="form-control" id="maker" name="maker" placeholder="Maker" value="">
         </div>
     </div>
     <div class="form-group">
         <label for="time_unit" class="col-sm-2 control-label">Time Unit</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" id="time_unit" placeholder="Time Unit">
+            <input type="text" class="form-control" id="time_unit" name="time_unit" placeholder="Time Unit">
         </div>
     </div>
     <div class="form-group">
         <label for="reservation_time" class="col-sm-2 control-label">Max Reservation Time</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" id="reservation_time" placeholder="Max Reservation Time">
+            <input type="text" class="form-control" id="reservation_time" name="reservation_time" placeholder="Max Reservation Time">
         </div>
     </div>
     <div class="form-group">
         <label for="price_per_unit" class="col-sm-2 control-label">Price per unit Time</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" id="price_per_unit" placeholder="Price per unit Time">
+            <input type="text" class="form-control" id="price_per_unit" name="price_per_unit" placeholder="Price per unit Time">
         </div>
     </div>
     <div class="form-group">
@@ -46,8 +46,13 @@
     <div class="form-group">
         <label for="assign_user" class="col-sm-2 control-label">Assign to Lab</label>
         <div class="col-sm-10">
-            <select name="assign_lab" id="assign_user" class="form-control" required="required">
+            <select name="assign_lab" id="assign_user"  name= "assign_user" class="form-control" required="required">
                 <option value="0">Choose Lab</option>
+                @if ($labs->count() > 0)
+                @foreach($labs as $lab)
+                <option value="{{ $lab->id }}">{{ $lab->title }}</option>
+                @endforeach
+                @endif
             </select>
         </div>
     </div>
@@ -59,7 +64,7 @@
     </div>
     <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10">
-            <button type="submit" class="btn btn-large btn-default">Save</button>
+            <button type="submit" class="btn btn-large btn-default" id="save-equipment">Save</button>
         </div>
     </div>
 </form>
