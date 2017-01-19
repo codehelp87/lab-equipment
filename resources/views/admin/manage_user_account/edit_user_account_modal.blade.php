e280ef19a06310c1d5d9777bc7301196759f5be7
Temitope Olotin <temitope.olotin@andela.com>
2017-01-20 00:03:38 +0100

    Edit user (#51)
    * Get user information
    
    * Get all users
    
    * Modifiy user acount
    
    * populate lab and user status
    
    * Set user account modal fields
    
    * Display user account status
    
    * Add reset password link to edit user modal
    
    * Modify user modal
    
    * Align user account listing
    
    * Fetch all user bookings
    
    * Edit user profile
    
    * Add the user edit form on modal
    
    * Arrange user accout properly
    
    * Remove user id

--- resources/views/admin/manage_user_account/edit_user_account_modal.blade.php
+++ resources/views/admin/manage_user_account/edit_user_account_modal.blade.php
@@ -3,13 +3,13 @@
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-				<h4 class="modal-title">Edit User | {{ $user->name }}</h4>
+				<h4 class="modal-title">Edit User's information</h4>
 			</div>
 			<div class="modal-body">
 			</div>
 			<div class="modal-footer">
-				<button type="button" class="btn btn-default no" data-dismiss="modal" id="{{ $user->id }}">No</button>
-				<button type="button" class="btn btn-default ok" id="{{ $user->id }}">Ok</button>
+				<button type="button" class="btn btn-default no" data-dismiss="modal">No</button>
+				<button type="button" class="btn btn-default ok">Ok</button>
 			</div>
 		</div>
 	</div>
