44bb94442c6b07857f9447f6dd6b24b9479db793
Temitope Olotin <temitope.olotin@andela.com>
2017-01-29 01:12:22 +0100

    Complete the booking

--- resources/views/student/my_profile.blade.php
+++ resources/views/student/my_profile.blade.php
@@ -65,14 +65,14 @@
                     <h5><strong>Upcoming Booking</strong> <span class="text-danger pull-right">Cancellation is allowed until 1hr. before your reservation</span></h5>
                     <table class="table table-hover">
                         <tbody>
-                         @if ($bookings->count() > 0)
+                            @if ($bookings->count() > 0)
                             <tr>
-                                <td><strong>{{ $bookings[count($bookings) - 1]->equipment->title }}</strong></td>
-                                <td>{{ $bookings[count($bookings) - 1]->equipment->model_no }}</td>
-                                <td>{{ date_format(new \DateTime($bookings[count($bookings) - 1]->booking_date), 'Y/m/d') }}</td>
-                                <td>@if (@$bookings[count($bookings) - 1]->time_slot != null) {{ implode(' , ', @$bookings[count($bookings) - 1]->time_slot) }}
+                                <td><strong>{{ $bookings[0]->equipment->title }}</strong></td>
+                                <td>{{ $bookings[0]->equipment->model_no }}</td>
+                                <td>{{ date_format(new \DateTime($bookings[0]->booking_date), 'Y/m/d') }}</td>
+                                <td>@if (@$bookings[0]->time_slot != null) {{ implode(' , ', @$bookings[0]->time_slot) }}
                                 @endif </td>
-                                <td><button type="button" class="btn btn-default pull-right">Cancel</button></td>
+                                <td><button type="button" class="btn btn-default pull-right cancel-booking" id="{{ $bookings[0]->equipment->id }}">Cancel</button></td>
                             </tr>
                             @endif
                         </tbody>
