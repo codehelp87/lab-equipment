e5ac7a4781673e586cdbac5c0a18dde2b9e9e022
Temitope Olotin <temitope.olotin@andela.com>
2017-01-31 07:56:10 +0100

    Fix bookng history

--- resources/views/student/partials/upcoming_booking.blade.php
+++ resources/views/student/partials/upcoming_booking.blade.php
@@ -1,26 +1,27 @@
 <h5><strong>Upcoming Booking</strong> <span class="text-danger pull-right">Cancellation is allowed until 1hr. before your reservation</span></h5> <br>
 <table class="table table-hover">
     <tbody>
-        @if ($bookings->count() > 0)
+        @foreach($bookings as $booking)
         <tr>
-            <td><strong>{{ $bookings[0]->equipment->title }}</strong></td>
-            <td>{{ $bookings[0]->equipment->model_no }}</td>
-            <td>{{ date_format(new \DateTime($bookings[0]->booking_date), 'Y/m/d') }}</td>
-            <td>@if (@$bookings[0]->time_slot != null) {{ implode(' , ', @$bookings[0]->time_slot) }}
+            <td><strong>{{ $booking->equipment->title }}</strong></td>
+            <td>{{ $booking->equipment->model_no }}</td>
+            <td>{{ date_format(new \DateTime($booking->booking_date), 'Y/m/d') }}</td>
+            <td>@if ($booking->time_slot != null) {{ implode(' , ', $booking->time_slot) }}
             @endif </td>
-            <?php $lastBookingTime = $bookings[0]->created_at->diffInMinutes( Carbon\Carbon::now()); ?>
+            <?php $lastBookingTime = $booking->created_at->diffInMinutes( Carbon\Carbon::now()); ?>
             <td>
-                @if ($lastBookingTime >= 60 && $bookings[0]->status == 1)
-                <button type="button" class="btn btn-default pull-right cancel-booking inActiveBtn" id="{{ $bookings[0]->id }}" disabled="disabled"> Cancel</button>
+                @if ($lastBookingTime >= 60 && $booking->status == 1)
+                <button type="button" class="btn btn-default pull-right cancel-booking inActiveBtn" id="{{ $booking->id }}" disabled="disabled"> Cancel</button>
                 @endif
-                @if($lastBookingTime < 60 && $bookings[0]->status == 1)
-                <?php $bookingSlot = []; if (!is_null($bookings[0]->time_slot)) { $bookingSlot = $bookings[0]->time_slot ;} ?>
-                <button type="button" class="btn btn-default pull-right cancel-booking" id="{{ $bookings[0]->id }}" data-time-slot="{{ implode(' , ', $bookingSlot) }}"> Cancel</button>
+                @if($lastBookingTime < 60 && $booking->status == 1)
+                <?php $bookingSlot = []; if (!is_null($booking->time_slot)) { $bookingSlot = $booking->time_slot ;} ?>
+                <button type="button" class="btn btn-default pull-right cancel-booking" id="{{ $booking->id }}" data-time-slot="{{ implode(' , ', $bookingSlot) }}"> Cancel</button>
                 @endif
-                @if($lastBookingTime <= 60 && $bookings[0]->time_slot == null)
+                @if($booking->time_slot == null && $booking->status == 0)
                 <button type="button" class="btn btn-default pull-right cancelled"> Cancelled</button>
                 @endif
-            </tr>
-            @endif
-        </tbody>
-    </table>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
