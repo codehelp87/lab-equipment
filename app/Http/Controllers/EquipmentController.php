89df8773f1ea90bfb992fc2519e40bf892fcc556
Temitope Olotin <temitope.olotin@andela.com>
2017-02-05 09:04:19 +0100

    dump id

--- app/Http/Controllers/EquipmentController.php
+++ app/Http/Controllers/EquipmentController.php
@@ -316,6 +316,7 @@ class EquipmentController extends Controller
         $bookingDate = date_format($date, 'Y-m-d');
 
         $decodedId = base64_decode($id);
+        dd($decodedId);
         $equipment = Equipment::find($decodedId);
         $equipmentBookings = Booking::findBy([
             ['equipment_id', '=', $id],
