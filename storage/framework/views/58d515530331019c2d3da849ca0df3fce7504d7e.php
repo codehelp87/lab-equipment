b6e7e5554aa7d6fee31ace743ffb995849df7a06
Temitope Olotin <temitope.olotin@andela.com>
2017-02-09 04:55:55 +0100

    fix login page

--- storage/framework/views/58d515530331019c2d3da849ca0df3fce7504d7e.php
+++ storage/framework/views/58d515530331019c2d3da849ca0df3fce7504d7e.php
@@ -5,52 +5,63 @@
             <div class="panel panel-default">
                 <div class="panel-heading text-center"><h4>Chemistry Dept. Equipment User System</h4></div>
                 <div class="panel-body">
-                    <?php if(session('message')): ?>
-                    <div class="alert alert-danger text-center">
-                        <?php echo e(session('message')); ?>
-
-                    </div>
-                    <?php endif; ?>
                     <form class="form-horizontal" role="form" method="POST" action="<?php echo e(url('/login')); ?>">
-                        <?php echo e(csrf_field()); ?>
+                        <div class="row">
+                            <div class="col-md-9">
+                                <?php if(session('message')): ?>
+                                <div class="alert alert-danger text-center">
+                                    <?php echo e(session('message')); ?>
 
-                        <div class="form-group<?php echo e($errors->has('email') ? ' has-error' : ''); ?>">
-                            <label for="email" class="col-md-4 control-label">ID</label>
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="<?php echo e(old('email')); ?>" required autofocus>
-                                <?php if($errors->has('email')): ?>
-                                <span class="help-block">
-                                    <strong><?php echo e($errors->first('email')); ?></strong>
-                                </span>
+                                </div>
                                 <?php endif; ?>
+                                
+                                    <?php echo e(csrf_field()); ?>
+
+                                    <div class="form-group<?php echo e($errors->has('email') ? ' has-error' : ''); ?>">
+                                        <label for="email" class="col-md-4 control-label">ID</label>
+                                        <div class="col-md-6">
+                                            <input id="email" type="email" class="form-control" name="email" value="<?php echo e(old('email')); ?>" required autofocus>
+                                            <?php if($errors->has('email')): ?>
+                                            <span class="help-block">
+                                                <strong><?php echo e($errors->first('email')); ?></strong>
+                                            </span>
+                                            <?php endif; ?>
+                                        </div>
+                                    </div>
+                                    <div class="form-group<?php echo e($errors->has('password') ? ' has-error' : ''); ?>">
+                                        <label for="password" class="col-md-4 control-label">PWD</label>
+                                        <div class="col-md-6">
+                                            <input id="password" type="password" class="form-control" name="password" required>
+                                            <?php if($errors->has('password')): ?>
+                                            <span class="help-block">
+                                                <strong><?php echo e($errors->first('password')); ?></strong>
+                                            </span>
+                                            <?php endif; ?>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="col-md-8 col-md-offset-4">
+                                            
+                                            <a class="btn btn-link" href="<?php echo e(route('request_training')); ?>">
+                                                Need to signup?
+                                            </a>
+                                        </div>
+                                    </div>
                             </div>
+                        
+                        <div class="col-md-3" style="margin-left: -15px;">
+                            <button type="submit" class="btn  btn-large btn-default" style="    margin-top: 15px;
+    float: left;
+    padding: 15px;
+    width: 120px;">
+                            Login
+                            </button>
                         </div>
-                        <div class="form-group<?php echo e($errors->has('password') ? ' has-error' : ''); ?>">
-                            <label for="password" class="col-md-4 control-label">PWD</label>
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-                                <?php if($errors->has('password')): ?>
-                                <span class="help-block">
-                                    <strong><?php echo e($errors->first('password')); ?></strong>
-                                </span>
-                                <?php endif; ?>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-default">
-                                Login
-                                </button>
-                                <a class="btn btn-link" href="<?php echo e(route('request_training')); ?>">
-                                    Need to signup?
-                                </a>
-                            </div>
-                        </div>
+                    </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<?php $__env->stopSection(); ?>
+    <?php $__env->stopSection(); ?>
 <?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
