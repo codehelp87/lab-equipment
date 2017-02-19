ce8e514c0d28cc3b3aafc30db4c640029255d8bc
Temitope Olotin <temitope.olotin@andela.com>
2017-02-19 19:18:07 +0100

    Add 70px to the margin-top

--- resources/views/admin/manage_user_account/logout.blade.php
+++ resources/views/admin/manage_user_account/logout.blade.php
@@ -1,4 +1,4 @@
-<p>
+<p style="margin-top: 70px;">
 	<h4 class="student"> Hello, {{ Auth::user()->name }}! <form action="/logout" method="post">
 		<button type="submit" class=" btn btn-default pull-right" style="margin-top: -20px;">Logout</button>
 		<input type="hidden" name="_token" id="_token" class="form-control" value="{{ csrf_token() }}">
--- storage/framework/views/e8b23c81718f648af1776ddfd399c29fb3d9973a.php
+++ storage/framework/views/e8b23c81718f648af1776ddfd399c29fb3d9973a.php
@@ -1,4 +1,4 @@
-<p>
+<p style="margin-top: 70px;">
 	<h4 class="student"> Hello, <?php echo e(Auth::user()->name); ?>! <form action="/logout" method="post">
 		<button type="submit" class=" btn btn-default pull-right" style="margin-top: -20px;">Logout</button>
 		<input type="hidden" name="_token" id="_token" class="form-control" value="<?php echo e(csrf_token()); ?>">
