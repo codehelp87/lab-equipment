caf5a49b6ecfa9595519ff8c8669763f32768771
Temitope Olotin <temitope.olotin@andela.com>
2017-02-21 01:45:19 +0100

    list all labs

--- resources/views/admin/manage_lab/manage_lab.blade.php
+++ resources/views/admin/manage_lab/manage_lab.blade.php
@@ -12,3 +12,28 @@
         </div>
     </div>
 </form>
+
+<div class="col-sm-8">
+<div class="table-responsive">
+    <table class="table table-responsive" id="list-labs">
+        <tbody>
+            @if($labs->count() > 0)
+            @foreach($labs as $lab)
+            <tr id="edit-lab{{ $lab->id }}">
+                <td>
+                   {{ $lab->title }}<br>
+                </td>
+                <td><a href="#" class="edit-lab" id="{{ $lab->id }}" title="{{ $lab->title }}">Edit</a></td>
+                <td><a href="#"  class="delete-lab" id="{{ $lab->id }}" rel= "{{ route('delete_equipment', ['id' => $lab->id ]) }}">Delete</a></td>
+            </tr>
+            <tr>
+                <td colspan="8">
+                    <div class="display{{ $lab->id }}" id="edit-eqipment{{ $lab->id }}" style="display: none;">dddjdjdjdjjddjdjdddjjdjjddjddjjddj</div>
+                </td>
+            </tr>
+            @endforeach
+            @endif
+        </tbody>
+    </table>
+    </div>
+</div>
--- storage/framework/views/bdbd68c14f8f6834a0dd44d95ba885cddf87fa5f.php
+++ storage/framework/views/bdbd68c14f8f6834a0dd44d95ba885cddf87fa5f.php
@@ -6,10 +6,34 @@
             <input type="text" class="form-control" id="title" name="title" placeholder="Lab Name">
         </div>
     </div>
-    
     <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10">
             <button type="submit" class="btn btn-large btn-default" id="save-lab">Save</button>
         </div>
     </div>
 </form>
+
+<div class="col-sm-8">
+<div class="table-responsive">
+    <table class="table table-responsive" id="list-labs">
+        <tbody>
+            <?php if($labs->count() > 0): ?>
+            <?php $__currentLoopData = $labs; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $lab): $__env->incrementLoopIndices(); $loop = $__env->getFirstLoop(); ?>
+            <tr id="edit-lab<?php echo e($lab->id); ?>">
+                <td>
+                   <?php echo e($lab->title); ?><br>
+                </td>
+                <td><a href="#" class="edit-lab" id="<?php echo e($lab->id); ?>" title="<?php echo e($lab->title); ?>">Edit</a></td>
+                <td><a href="#"  class="delete-lab" id="<?php echo e($lab->id); ?>" rel= "<?php echo e(route('delete_equipment', ['id' => $lab->id ])); ?>">Delete</a></td>
+            </tr>
+            <tr>
+                <td colspan="8">
+                    <div class="display<?php echo e($lab->id); ?>" id="edit-eqipment<?php echo e($lab->id); ?>" style="display: none;">dddjdjdjdjjddjdjdddjjdjjddjddjjddj</div>
+                </td>
+            </tr>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getFirstLoop(); ?>
+            <?php endif; ?>
+        </tbody>
+    </table>
+    </div>
+</div>
