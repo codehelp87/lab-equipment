0769457aab04566fc890e0ec6b0bd5784ca53206
Temitope Olotin <temitope.olotin@andela.com>
2017-02-21 04:22:15 +0100

    set margin-top of login page

--- storage/framework/views/58d515530331019c2d3da849ca0df3fce7504d7e.php
+++ storage/framework/views/58d515530331019c2d3da849ca0df3fce7504d7e.php
@@ -2,7 +2,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
+            <div class="panel panel-default" style="margin-top: 100px;">
                 <div class="panel-heading text-center"><h4>Chemistry Dept. Equipment User System</h4></div>
                 <div class="panel-body">
                         <div class="row">
