5d648d1a42e17f4881902113663b9d57fbd3e04e
Temitope Olotin <temitope.olotin@andela.com>
2017-02-22 08:50:32 +0100

    fix night booking

--- app/Http/Controllers/EquipmentController.php
+++ app/Http/Controllers/EquipmentController.php
@@ -541,7 +541,11 @@ class EquipmentController extends Controller
             } else {
                 foreach($nightBooking as $key => $booking) {
                     if (is_array($booking) && is_array($dayBooking)) {
-                        $newArray[] = array_merge(@$booking, @$dayBooking[$i]);
+                        if (count($dayBooking) > 0) {
+                            $newArray[] = array_merge(@$booking, @$dayBooking[$i]);
+                        } else {
+                            $newArray[] = @$booking;
+                        }
                     }
                     $i++;
                 }
