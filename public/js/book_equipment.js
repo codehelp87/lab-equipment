6e43847aab44e0b410d2ab60bd759be39a0538d1
Temitope Olotin <temitope.olotin@andela.com>
2017-02-23 05:07:15 +0100

    check button size

--- public/js/book_equipment.js
+++ public/js/book_equipment.js
@@ -89,6 +89,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
         var MAX_BOOKING_AHEAD = 30;
 
         var bookBtn = $(document).find('button#book-now');
+        console.log(bookBtn.size());
 
         bookBtn.on('click', function () {
           var equipment = new Equipment();
