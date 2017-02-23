fd09f3aad6d13981a7b68a1b3ddc7e7a3707b696
Temitope Olotin <temitope.olotin@andela.com>
2017-02-23 22:39:03 +0100

    bold click here

--- resources/views/student/complete_training_email.blade.php
+++ resources/views/student/complete_training_email.blade.php
@@ -7,7 +7,7 @@
 				Congratulations!
 You have completed the training session and you can now use the following equipment..<br><br>
 				Equipment: {{ $equipment }}<br><br>
-				Please <a href="<?php echo $_SERVER['HTTP_HOST'];?>/users/{{ base64_encode($email) }}/activate">click here</a> to update your account informaion.
+				Please <a href="<?php echo $_SERVER['HTTP_HOST'];?>/users/{{ base64_encode($email) }}/activate"><strong>CLICK HERE</strong></a> to update your account informaion.
 				Thanks,<br><br>
 				The Equipment Administrator
 			</p>
