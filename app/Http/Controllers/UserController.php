1c5256817c9c04d8305d4e7ccf3e98af9c014d93
Temitope Olotin <temitope.olotin@andela.com>
2017-02-24 18:25:32 +0100

    fix incorrect password text

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -220,7 +220,7 @@ class UserController extends Controller
             }
         }
 
-        return response()->json(['message' => 'Error updating password']);
+        return response()->json(['message' => 'The current password is Incorrect']);
     }
 
     public function viewMyProfile()
