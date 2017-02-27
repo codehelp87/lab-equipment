a4b03edd49db89dc051e8d9dca322be612dff8c7
Temitope Olotin <temitope.olotin@andela.com>
2017-02-27 22:24:29 +0100

    show training

--- app/Http/Controllers/EquipmentController.php
+++ app/Http/Controllers/EquipmentController.php
@@ -267,10 +267,9 @@ class EquipmentController extends Controller
                 }
             }
 
-            //$sortedStudents = $this->array_sort($students, 'accepted', $order = SORT_DESC);
             $totalStudents = count($students);
 
-            return response()->json(['draw' => 5, 'recordsTotal' => $totalStudents, 'recordsFiltered' => $totalStudents,  'data'  => $students], 200);
+            return response()->json(['draw' => 5, 'recordsTotal' => $totalStudents, 'recordsFiltered' => $totalStudents,  'data' => $students], 200);
         }
 
         return response()->json([
@@ -313,8 +312,6 @@ class EquipmentController extends Controller
                 }
             }
 
-            //$sortedStudents = $this->array_sort($students, 'accepted', $order = SORT_DESC);
-            
             $totalStudents = count($students);
             return response()->json(['draw' => 5, 'recordsTotal' => $totalStudents, 'recordsFiltered' => $totalStudents,  'data'  => $students], 200);
         }
@@ -513,40 +510,5 @@ class EquipmentController extends Controller
             ->first();
 
         return $completedTrainingRequests;
-    }
-
-    protected function array_sort($array, $on, $order = SORT_ASC)
-    {
-        $new_array = array();
-        $sortable_array = array();
-
-        if (count($array) > 0) {
-            foreach ($array as $k => $v) {
-                if (is_array($v)) {
-                    foreach ($v as $k2 => $v2) {
-                        if ($k2 == $on) {
-                            $sortable_array[$k] = $v2;
-                        }
-                    }
-                } else {
-                    $sortable_array[$k] = $v;
-                }
-            }
-
-            switch ($order) {
-                case SORT_ASC:
-                    asort($sortable_array);
-                    break;
-                case SORT_DESC:
-                    arsort($sortable_array);
-                    break;
-            }
-
-            foreach ($sortable_array as $k => $v) {
-                $new_array[$k] = $array[$k];
-            }
-        }
-
-        return $new_array;
     }
 }
