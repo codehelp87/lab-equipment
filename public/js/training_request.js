79022df7c6a17d3e8daf782de6be4b439879e84f
Temitope Olotin <temitope.olotin@andela.com>
2017-02-27 09:00:15 +0100

    load more data

--- public/js/training_request.js
+++ public/js/training_request.js
@@ -280,7 +280,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
           var route = '/equipments/' + equipmentId + '/students';
           if (_this.val() != '') {
             req.makeAjaxCall(route, '', 'GET').done(function (data) {
-              if (data[1].length > 0) {
+              if (data[1].length > 0 || data[1].length == undefined) {
                 var students = req.displayTrainingRequest(data);
                 tableBody.html(students);
 
