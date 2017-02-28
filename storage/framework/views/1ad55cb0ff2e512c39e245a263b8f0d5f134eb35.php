5c04519aebefc9077b387f02c0323bca14ef3048
Temitope Olotin <temitope.olotin@andela.com>
2017-02-28 05:43:46 +0100

    render view

---
+++ storage/framework/views/1ad55cb0ff2e512c39e245a263b8f0d5f134eb35.php
@@ -0,0 +1,16 @@
+<div class="modal fade contact-admin"  id="contact-admin" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+	<div class="modal-dialog modal-sm" role="document">
+		<div class="modal-content">
+		<div class="modal-body">
+			<span class="text-center"><strong>Please contact the admin via email</strong></span> <br><br>
+			<address>
+			    <strong>Name:</strong> Diwesh Saxena <br>
+				<strong>Email:</strong> diweshsaxena@gmail.com
+			</address>
+		</div>
+			<div class="modal-footer">
+			<button type="button" class="btn btn-default no" data-dismiss="modal">Close</button>
+		</div>
+		</div>
+	</div>
+</div>
